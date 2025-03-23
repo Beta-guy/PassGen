920cb09122bf09cde2a8bf3e2f69df1cc471777a
Stephanie <beta-guy@proton.me>
2025-03-23 14:00:37 -0600

    Ii actually generates Passwords!

--- passgenGUI.py
+++ passgenGUI.py
@@ -1,38 +1,56 @@
 #imports
-from tkinter import *
-import subprocess
-import sys
 import random
+from tkinter import messagebox, Tk, Entry, Label, Checkbutton, IntVar, Button
+
+
 #collecting Information
 def save_input():
-    system = sys.platform
     site_value = site.get()
     username_value = username.get()
+    char_lists = ""
+    if special_val.get() == 1:
+        char_lists += spe
+    if low_val.get() == 1:
+        char_lists += low
+    if upp_val.get() == 1:
+        char_lists += upp
+    if num_val.get() == 1:
+        char_lists += numbers
+    if space_val.get() == 1:
+        char_lists += spa
+    password = ""
     max_value = max_text.get()
-    match max_value:
-        case "":
-            max_value = "12"
-        case type("test"):
-            messagebox.showinfo("Password Length", "Invalid Password Length")
-            exit(0)
-        case _:
-            max_value = max_text.get()
-    special_value = special_val.get()
-    match special_value:
-        case 1:
-            special_value = "yes"
-        case 0:
-            special_value = "no"
+    if max_value == '':
+        max_value = 12
+    elif max_value.isnumeric() == 0:
+        messagebox.showinfo("Type Error", "Numbers only please!")
+    else:
+        max_value = int(max_text.get())
+    if char_lists == "":
+        char_lists = upp+low+numbers+spe
+    for num in range(int(max_value)):
+        password += random.choice(char_lists)
+    messagebox.showinfo("Password", password)
 #defining list types
-low = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-upp = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-num = [1,2,3,4,5,6,7,8,9,0]
-spe = ["!","@","#","$","%","^","&","*","(",")","?","<",">"]
-spa = [" "]
+low = "abcdefghijklmnopqrstuvwxyz"
+upp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+numbers = "1234567890"
+spe = "!@#$%^&*()-+<>?"
+spa = " "
+
+
+
+
+
+
+
+
+
+
 #GUI Fun!
 root = Tk()
 #rnd title
-title = random.sample(low+upp+num+spe+spa, k=12)
+title = random.sample(low+upp+numbers+spe+spa, k=12)
 root.title(title)
 
 #window size
@@ -62,18 +80,19 @@ low_check = Checkbutton(root, text="Lowercase?", variable=low_val)
 low_check.place(x=160, y=120)
 
 upp_val = IntVar()
-upp_check = Checkbutton(root, text="Uppercase?", variable=special_val)
+upp_check = Checkbutton(root, text="Uppercase?", variable=upp_val)
 upp_check.place(x=160, y=100)
 
 num_val = IntVar()
-num_check = Checkbutton(root, text="Numbers?", variable=special_val)
+num_check = Checkbutton(root, text="Numbers?", variable=num_val)
 num_check.place(x=400, y=120)
 
 space_val = IntVar()
-space = Checkbutton(root, text="Space Chars?", variable=special_val)
+space = Checkbutton(root, text="Space Chars?", variable=space_val)
 space.place(x=280, y=120)
 
 button = Button(root, text="Generate Password", command=save_input)
 button.place(x=180, y=160)
 
 root.mainloop()
+
