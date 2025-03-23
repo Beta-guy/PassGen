8f45346cdc1c566bbd6e1e3237776765ea409598
Stephanie <beta-guy@proton.me>
2025-03-23 11:29:26 -0600

    More password options

--- passgenGUI.py
+++ passgenGUI.py
@@ -37,26 +37,41 @@ root.title(title)
 
 #window size
 root.geometry("500x200")
-
+# get Site Name
 site = Entry(root, width=30)
 site.place(x=20, y=20)
 site_label = Label(root, text="Site or Application")
 site_label.place(x=280, y=20)
-
+#get username
 username = Entry(root, width=30)
 username.place(x=20, y=70)
 username_label = Label(root, text="Username or Email Address")
 username_label.place(x=280, y=70)
-
+#get Password length
 max_text = Entry(root, width=3)
 max_text.place(x=20, y=120)
 max_label = Label(root, text="Password Length")
 max_label.place(x=60, y=120)
-
+#get password attributes
 special_val = IntVar()
 special = Checkbutton(root, text="Special Chars?", variable=special_val)
-special.place(x=280, y=120)
+special.place(x=280, y=100)
 
+low_val = IntVar()
+low_check = Checkbutton(root, text="Lowercase?", variable=low_val)
+low_check.place(x=160, y=120)
+
+upp_val = IntVar()
+upp_check = Checkbutton(root, text="Uppercase?", variable=special_val)
+upp_check.place(x=160, y=100)
+
+num_val = IntVar()
+num_check = Checkbutton(root, text="Numbers?", variable=special_val)
+num_check.place(x=400, y=120)
+
+space_val = IntVar()
+space = Checkbutton(root, text="Space Chars?", variable=special_val)
+space.place(x=280, y=120)
 
 button = Button(root, text="Generate Password", command=save_input)
 button.place(x=180, y=160)
